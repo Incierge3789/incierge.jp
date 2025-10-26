3042c490651b068db966b45810681390425a1497
inicierge3789 <silver.and.gold13579@gmail.com>
2025-10-26 21:04:33 +0900

    chore: enable sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: 'https://incierge.jp',
-  integrations: [
-    tailwind({ applyBaseStyles: false }),
-    mdx()
-  ],
+  site: "https://incierge.jp",
+  integrations: [tailwind({ applyBaseStyles: false }), mdx(), sitemap()],
 });
