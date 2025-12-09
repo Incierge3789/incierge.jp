4f46c433836aea5b3855f27b20b655efe6949e0a
inicierge3789 <silver.and.gold13579@gmail.com>
2025-12-09 17:02:51 +0900

    Enable Tailwind typography plugin (fix import)

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import typography from '@tailwindcss/typography';
+
 export default {
   content: ['./src/**/*.{astro,html,md,mdx,js,jsx,ts,tsx}'],
   theme: {
