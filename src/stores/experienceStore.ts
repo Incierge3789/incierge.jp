a57eabe6064eec075b1b49e61d066da09073c924
inicierge3789 <silver.and.gold13579@gmail.com>
2025-12-17 15:12:16 +0900

    fix: unify diagnosis->experience->cta into single conversion flow

--- src/stores/experienceStore.ts
+++
@@ -1,7 +0,0 @@
-import { atom } from 'nanostores';
-import type { Persona } from '../lib/experience/generateScheduleEmail';
-
-export const personaAtom = atom<Persona>('tax_accountant');
-export const rateAtom = atom<number>(6000);
-export const countAtom = atom<number>(20);
-export const isSolutionRevealedAtom = atom<boolean>(false);
